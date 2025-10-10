03a910bb1fc962b94d0fbfa3a5240780a0f246db
Ceo-Sammarco <noah.kd@gmail.com>
2025-10-10 15:15:36 -0300

    @Ceo-Sammarco - 10/10/2025

---
+++ assets/tags/List.jsx
@@ -0,0 +1,10 @@
+
+export default function List( { items } ) {
+  return( <>
+    <ul>
+      { items.map( item => (
+        <li key={ item.id }>{ item.name }</li>
+      ))}
+    </ul>
+  </> );
+}
